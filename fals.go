576c9701aeef5a0c74a265d1ca50b7836703972f
Miiro <miiro.juuso@artirix.com>
2016-01-18 13:31:43 +0000

    feat(firehose): set maximum record size

--- fals.go
+++ fals.go
@@ -9,7 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/firehose"
-	"github.com/foize/go.fifo"
+	"github.com/mjuuso/go.fifo"
 	"os"
 	"time"
 )
@@ -18,7 +18,7 @@ type Configuration struct {
 	Api_endpoint, Api_key, Project, Env        string
 	Aws_region, Aws_access_key, Aws_secret_key string
 	Firehose_stream_name                       string
-	Shipping_interval                          int
+	Shipping_interval, Firehose_record_size    int
 	Components                                 []Component
 }
 
@@ -47,11 +47,12 @@ func main() {
 	// initialize aws sdk and credentials from configuration
 	svc := firehose.New(session.New(), &aws.Config{Region: aws.String(configuration.Aws_region),
 		Credentials: credentials.NewStaticCredentials(configuration.Aws_access_key, configuration.Aws_secret_key, "")})
-	fmt.Println("region", configuration.Aws_region)
-	fmt.Println(svc)
-	streams, err := svc.ListDeliveryStreams(&firehose.ListDeliveryStreamsInput{})
-	fmt.Println(streams)
-	fmt.Println(err)
+
+	_, err = svc.ListDeliveryStreams(&firehose.ListDeliveryStreamsInput{})
+	if err != nil {
+		fmt.Println("Error listing delivery streams:", err)
+		return
+	}
 
 	// start Filewatchers for the files
 	for _, comp := range configuration.Components {
@@ -67,39 +68,54 @@ func main() {
 			var data = bytes.NewBuffer([]byte(``))
 			encoder := json.NewEncoder(data)
 
-			// add queue contents as payload
+			// add queue contents as payload, not exceeding Firehose_record_size
 			for {
-				item := outgoing.Next()
+				// get the next item in the queue
+				item := outgoing.Peek()
 				if item == nil {
+					// no more items in queue
 					break
 				}
 
-				error := encoder.Encode(item)
+				json_value, err := json.Marshal(item)
+				if err != nil {
+					fmt.Println("Encoding error:", err)
+					break
+				}
+
+				if (bytes.NewBuffer(json_value).Len() + data.Len()) > configuration.Firehose_record_size {
+					// record size would be exceeded
+					break
+				}
+
+				// encode the item to payload buffer, while removing it from the queue
+				error := encoder.Encode(outgoing.Next())
 
 				if error != nil {
-					fmt.Println("Encoding error: ", error)
+					fmt.Println("Encoding error:", error)
 				}
 
 			}
-			fmt.Println("Buf", data.String())
 
+			// create the Firehose record
 			params := &firehose.PutRecordInput{
-				DeliveryStreamName: aws.String(configuration.Firehose_stream_name), // Required
-				Record: &firehose.Record{ // Required
-					Data: data.Bytes(), // Required
+				DeliveryStreamName: aws.String(configuration.Firehose_stream_name),
+				Record: &firehose.Record{
+					Data: data.Bytes(),
 				},
 			}
+
+			// send the record to Firehose
 			resp, err := svc.PutRecord(params)
 
 			if err != nil {
-				// Print the error, cast err to awserr.Error to get the Code and
-				// Message from an error.
 				fmt.Println(err.Error())
 				return
 			} else {
 				fmt.Println(resp)
 			}
 		}
+		// sleep Shipping_interval before the next iteration
 		time.Sleep(time.Duration(configuration.Shipping_interval) * time.Second)
 	}
 }
